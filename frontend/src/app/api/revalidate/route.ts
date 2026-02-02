db243156f58a1987425f5cdc665b8b09da1968e1
k_czech <kamczeh@gmail.com>
2026-02-02 08:36:07 +0100

    refactor: revalidate webhook

--- frontend/src/app/api/revalidate/route.ts
+++ frontend/src/app/api/revalidate/route.ts
@@ -2,15 +2,19 @@ import { revalidatePath } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
-  const secret = request.nextUrl.searchParams.get('secret');
+  const { searchParams } = new URL(request.url);
+  const secret = searchParams.get('secret');
+
+  console.log("Otrzymany secret:", secret);
+  console.log("Oczekiwany secret z ENV:", process.env.REVALIDATION_TOKEN);
+
   if (secret !== process.env.REVALIDATION_TOKEN) {
     return NextResponse.json({ message: 'Invalid token' }, { status: 401 });
   }
 
   try {
-    revalidatePath('/', 'layout'); 
-    
-    return NextResponse.json({ revalidated: true, now: Date.now() });
+    revalidatePath('/', 'layout');
+    return NextResponse.json({ revalidated: true });
   } catch (err) {
     return NextResponse.json({ message: 'Error revalidating' }, { status: 500 });
   }
